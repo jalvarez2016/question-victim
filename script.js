14df65340c84dc8a265b92735fb138c82920a7a8
Jason <alvarezcoraiza@citypolyhs.org>
2017-03-27 20:28:25 +0000

    a random guesser

---
+++ script.js
@@ -0,0 +1,35 @@
+/* global $*/
+$(document).ready(function() {
+
+
+var ScriptEders = [
+    "Usman",
+    "Barry",
+    "David",
+    "Aldair",
+    "Nat",
+    'Taeiq',
+    'Kaysten',
+    'Jason',
+];
+
+var ScriptEducators = [
+  'Micheal',
+  'Thai',
+  'Josh',
+  'David',
+];
+
+      $("#child").click(function(){
+          
+          $('#student').append(ScriptEders[Math.floor(Math.random()*8)] + ' ANSWER MY QUESTIONS!!!!');
+          
+      });
+ 
+ 
+      $("#adult").click(function(){
+        
+          $('#teacher').append(ScriptEducators[Math.floor(Math.random()*4)] + ' ANSWER MY QUESTIONS!!!!');
+ 
+      });
+});
